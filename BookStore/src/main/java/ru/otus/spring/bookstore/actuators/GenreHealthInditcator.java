948ab9673f5e321c06c815a36a7b49d2dbd64bab
SergeyKlunichenko <sergey.klunichenko.ekaterinburg@gazprombank.ru>
2021-10-22 07:27:42 +0500

    Добавил логи в HealthIndicator

--- BookStore/src/main/java/ru/otus/spring/bookstore/actuators/GenreHealthInditcator.java
+++ BookStore/src/main/java/ru/otus/spring/bookstore/actuators/GenreHealthInditcator.java
@@ -1,5 +1,9 @@
 package ru.otus.spring.bookstore.actuators;
 
+import org.apache.logging.log4j.spi.LoggerRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.actuate.health.Status;
@@ -12,7 +16,7 @@ import ru.otus.spring.bookstore.repositories.GenreRepository;
 public class GenreHealthInditcator implements HealthIndicator {
 
     private  final GenreRepository genreRepository;
-
+    private  final Logger logger = LoggerFactory.getLogger(this.getClass());
     public GenreHealthInditcator(GenreRepository genreRepository) {
         this.genreRepository = genreRepository;
     }
@@ -21,15 +25,20 @@ public class GenreHealthInditcator implements HealthIndicator {
     public Health health() {
         String name = "контрольный";
 
+        logger.info("Проверка наличия контрольного жанра");
+
         Genre genre = genreRepository.findByName(name);
 
         if(genre == null){
+            logger.info("Контрольный жанр отсуствует");
             return Health.down()
                     .status(Status.DOWN)
                     .withDetail("message", String.format("Контрольный жанр не найден в списке жанров"))
                     .build();
 
         }
+
+        logger.info("Контрольный жанр найден");
 
         return Health.up()
                 .status(Status.UP)
